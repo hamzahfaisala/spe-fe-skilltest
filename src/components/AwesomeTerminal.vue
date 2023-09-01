2d331082afe5b6fae2f037275d7f89fe93e6ad96
Hamzah Faisal Azmi <hfaisal3@pmintl.net>
2023-09-01 15:49:57 +0700

    date and time

--- src/components/AwesomeTerminal.vue
+++ src/components/AwesomeTerminal.vue
@@ -1,11 +1,37 @@
 <template>
   <div>
-    <div class="rectangle"></div>
+    <div class="rectangle">
+      <div class="date-container">
+        <span style="width: 100;" class="date">{{timestamp}}</span>
+      </div>
+      <h1 class="title">&lt;SPE FRONTEND&gt</h1>
+    </div>
     <div class="rainbow">
-      <h1 class="title">helo</h1>
     </div>
   </div>
 </template>
+<script>
+ 
+ export default {
+  created() {
+    setInterval(this.currentDate, 1000);
+  },
+  data() {
+    return {
+      timestamp:''
+    }
+  },
+  methods: {
+    currentDate() {
+      const today = new Date();
+      const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+      const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+      const dateTime = date +" "+ time;
+      this.timestamp = dateTime;
+    }
+  },
+ };
+</script>
 <style>
   .rectangle {
     height: 800px;
@@ -16,10 +42,32 @@
     margin-top: -500px;
     border-radius: 50px;
     border: 30px solid #EEEEEE;
+    display: flex;
+  }
+
+  .date-container{
+    width: 100px;
+    bottom: 0px;
+    color: #00FF00;
+    align-self: flex-end;
+    font-size: 20px;
+    margin-bottom: 200px;
+    font-weight: 300; 
+    transform: rotate(90deg);
+  }
+
+  .date{
+    white-space: nowrap;
   }
 
   .title{
-
+    bottom: 0px;
+    color: #00FF00;
+    align-self: flex-end;
+    margin: 10px;
+    margin-left: 100px;
+    font-size: 80px;
+    font-weight: 300;
   }
 
   .rainbow{
